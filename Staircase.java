c33fb7df33f26cbc18349867e0a36dd8e666260c
Mahamad Abdulah <126168630+Mahamadabdulah2025@users.noreply.github.com>
2023-03-07 19:06:57 +0530

    Create Staircase.java

---
+++ Staircase.java
@@ -0,0 +1,62 @@
+/*Sample Input
+
+6 
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n=6 */
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+    // Write your code here
+           for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                    System.out.print(" ");
+        }
+         for(int k=n-i;k<n;k++){
+                    System.out.print("#");
+        }
+            System.out.println();
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
+
+
+
